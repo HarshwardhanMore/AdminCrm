b354ef1933e438b52155544bfe4e78c6517ca85b
Harshwardhan More <harshawardhanmore14@gmail.com>
2024-01-08 17:34:31 +0530

    trash added

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,8 @@ const router = require('express').Router();
 
 module.exports = (upload) =>{
 
-    router.use('/user', require('./contact'));
-    router.use('/user', require('./userEnquiry'));
-    router.use('/user', require('./enterpriseEnquiry'));
-    router.use('/user', require('./newsletter'));
     router.use('/admin', require('./admin')(upload));
+    router.use('/admin/permission', require('./permission'));
 
     return router;
 }
